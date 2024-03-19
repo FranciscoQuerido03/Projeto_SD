7f3a78c8c3eacfe759446f1c9153e31b4806be7c
Francisco Querido <148355011+FranciscoQuerido03@users.noreply.github.com>
2024-03-19 14:23:34 +0000

    Downloaders First Version

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.rmi.Naming;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+    public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
+        QueueInterface queue = (QueueInterface) Naming.lookup("rmi://localhost/queue");
+        ArrayList<String> urls = new ArrayList<>();
+
+//        for (int i = 0; i < queue.size(); i++) {
+//            urls.add(queue.getFirst());
+//        }
+
+        urls.add("https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_brasileiros_por_%C3%A1rea_decrescente");
+        urls.add("https://www.facebook.com");
+        urls.add("https://www.twitter.com");
+        urls.add("https://www.instagram.com");
+        urls.add("https://www.linkedin.com");
+        urls.add("https://www.pinterest.com");
+        urls.add("https://www.snapchat.com");
+        urls.add("https://www.reddit.com");
+        urls.add("https://www.tumblr.com");
+        urls.add("https://www.flickr.com");
+        urls.add("https://www.google.com");
+
+        int urls_size = urls.size();
+
+        for (int i = 0; i < urls_size; i++) {
+            String url = urls.removeFirst();
+            new Downloader(url);
         }
+
     }
 }
