d793f4b18f753f35ba52181331f513f5f31fe4aa
Francisco Querido <148355011+FranciscoQuerido03@users.noreply.github.com>
2024-03-21 00:34:26 +0000

    Barrels Config File

--- src/sd_projeto/IndexBarrels.java
+++ src/sd_projeto/IndexBarrels.java
@@ -1,5 +1,6 @@
 package sd_projeto;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.*;
 import java.net.UnknownHostException;
@@ -15,55 +16,49 @@ import java.util.List;
 public class IndexBarrels extends UnicastRemoteObject implements Barrel_I {
 
 	static HashMap<String, List<String>> hashMap = new HashMap<>();
-	public static Request Conection;
+	static HashMap<String, Integer> urls = new HashMap<>();
+	static HashMap<String, Integer> links = new HashMap<>();
 
-	private static String MULTICAST_ADDRESS = "224.3.2.1";
-	private static int PORT = 4321;
+	public static Request Conection;
+	private static String NAMING_URL= "";
+	private static String MULTICAST_ADDRESS = "";
+	private static int PORT = 0;
 	
 	public IndexBarrels() throws RemoteException {
 		super();
 
-		List<String> list1 = new ArrayList<>();
-        list1.add("Ola");
-        list1.add("teste");
-        list1.add("wtf");
-        hashMap.put("google.com", list1);
-
-        List<String> list2 = new ArrayList<>();
-        list2.add("banks");
-        list2.add("tedx");
-        hashMap.put("tedx.com", list2);
 	}
 
 	public void request(String m) throws java.rmi.RemoteException {
 		String[] words = m.split(" ");
 
-		for (String word : words) {
-			Urls_list urls = searchUrls(word);
-
-			Conection.answer(urls);
-
-		}
+//		for (String word : words) {
+//			//Urls_list urls = searchUrls(word);
+//
+//			Conection.answer(urls);
+//
+//		}
 	}
 
-	public Urls_list searchUrls(String palavra) {
-		List<String> chavesEncontradas = new ArrayList<>();
-		for (String chave : hashMap.keySet()) {
-			List<String> lista = hashMap.get(chave);
-			if (lista.contains(palavra)) {
-				chavesEncontradas.add(chave);
-			}
-		}
-        return new Urls_list(chavesEncontradas);
-	}
+//	public Urls_list searchUrls(String palavra) {
+////		List<String> chavesEncontradas = new ArrayList<>();
+////		for (String chave : hashMap.keySet()) {
+////			List<String> lista = hashMap.get(chave);
+////			if (lista.contains(palavra)) {
+////				chavesEncontradas.add(chave);
+////			}
+////		}
+////        return new Urls_list(chavesEncontradas);
+//	}
 
 	// =======================================================
 
 	public static void main(String[] args) {
+		readFile();
 		MulticastSocket socket = null;
 
 		try {
-			Conection = (Request) Naming.lookup("rmi://localhost:1099/request_barrel");
+			Conection = (Request) Naming.lookup(NAMING_URL);
 			IndexBarrels h = new IndexBarrels();
 			Conection.subscribe((Barrel_I) h);
 
@@ -94,6 +89,19 @@ public class IndexBarrels extends UnicastRemoteObject implements Barrel_I {
         }
 
     }
+
+	private static void readFile() {
+		File file = new File("src\\Index_config.txt");
+		try {
+			java.util.Scanner scanner = new java.util.Scanner(file);
+			NAMING_URL = scanner.nextLine();
+			MULTICAST_ADDRESS = scanner.nextLine();
+			PORT = Integer.parseInt(scanner.nextLine());
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	private static void DealPacket(DatagramPacket packet) {
 		String message = new String(packet.getData(), 0, packet.getLength());
