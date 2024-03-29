71c6193dd1265e7b03bd8a7e0e5cd051e2dc8841
Francisco Querido <148355011+FranciscoQuerido03@users.noreply.github.com>
2024-03-29 21:42:03 +0000

    Downloader thread safe and bloom filter done

---
+++ src/sd_projeto/BloomFilter.java
@@ -0,0 +1,41 @@
+/*
+Codigo da autoria de:
+Oliver Foster
+https://medium.com/javarevisited/java-how-to-implement-a-bloom-filter-and-what-is-it-used-for-070632cbe7a9
+ */
+
+package sd_projeto;
+
+import java.util.BitSet;
+import java.util.function.Function;
+
+public class BloomFilter<T> {
+    private BitSet bitSet;
+    private int size;
+    private Function<T, Integer>[] hashFunctions;
+
+    @SuppressWarnings("unchecked")
+    public BloomFilter(int size, Function<T, Integer>... hashFunctions) {
+        this.size = size;
+        this.bitSet = new BitSet(size);
+        this.hashFunctions = hashFunctions;
+    }
+
+    public void add(T element) {
+        for (Function<T, Integer> hashFunction : hashFunctions) {
+            int hash = Math.abs(hashFunction.apply(element) % size);
+            bitSet.set(hash, true);
+        }
+    }
+
+    public boolean contains(T element) {
+        if (element == null) return false;
+        for (Function<T, Integer> hashFunction : hashFunctions) {
+            int hash = Math.abs(hashFunction.apply(element) % size);
+            if (!bitSet.get(hash)) {
+                return false; // Any bit being 0 means the element is definitely not in the set
+            }
+        }
+        return true; // Possibly in the set
+    }
+}
--- src/sd_projeto/Downloader.java
+++ src/sd_projeto/Downloader.java
@@ -10,44 +10,71 @@ import java.net.*;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Downloader extends Thread {
     private static final String MULTICAST_ADDRESS = "224.3.2.1";
     private static final int PORT = 4321;
     private static InetAddress group;
-    private static final int NUM = 5;
-    private String url;
+    private static final int NUM = 2;
     private static QueueInterface queue;
+    private static BloomFilter<String>  bloomFilter;
+    private static final Lock lock = new ReentrantLock();
 
-    public Downloader() throws RemoteException {
+    public Downloader(int downloaderNumber) throws RemoteException {
+        setName("Downloader " + downloaderNumber);
         start();
     }
 
     @Override
     public void run() {
+        boolean flag;
+        String url;
+
         try {
             while (true) {
                 url = queue.getFirst();
+                if(url != null) {
+                    lock.lock();
+                    flag = bloomFilter.contains(url);
+                    lock.unlock();
 
-                if (url != null) {
-                    try {
-                        MulticastSocket socket = new MulticastSocket();
-                        Document doc = Jsoup.connect(url).get();
-                        Elements links = doc.select("a[href]");
-                        print("\nLinks: (%d)", links.size());
+                    if (!flag) {
+                        try {
+                            MulticastSocket socket = new MulticastSocket();
+                            Document doc = Jsoup.connect(url).get();
+                            Elements links = doc.select("a[href]");
+                            print("\nLinks: (%d)", links.size());
 
-                        for (Element link : links) {
-                            String message = link.attr("abs:href") + " " + link.text();
-                            byte[] buffer = message.getBytes();
-                            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
-                            socket.send(packet);
-                            System.out.println("Sent message: " + message);
+                            for (Element link : links) {
+                                String linkUrl = link.attr("abs:href");
 
-                            queue.addLast(link.attr("abs:href"));
+                                lock.lock();
+                                flag = bloomFilter.contains(linkUrl);
+                                lock.unlock();
+
+                                if (!flag) {
+                                    String message = linkUrl + " " + link.text();
+                                    byte[] buffer = message.getBytes();
+                                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
+                                    socket.send(packet);
+                                    System.out.println("Sent message: " + message);
+
+                                    queue.addLast(linkUrl);
+
+                                    lock.lock();
+                                    bloomFilter.add(linkUrl);
+                                    lock.unlock();
+                                }
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    }
+                    else{
+                        System.out.println("URL already visited: " + url);
                     }
                 }
             }
@@ -60,10 +87,21 @@ public class Downloader extends Thread {
         queue = (QueueInterface) Naming.lookup("rmi://localhost:1096/request_downloader");
         group = InetAddress.getByName(MULTICAST_ADDRESS);
 
+        bloomFilter = new BloomFilter<>(1328771238,s -> s.hashCode(),s -> s.hashCode() * s.length());
+
         for (int i = 0; i < NUM; i++) {
-            new Downloader();
-            System.out.println("Downloader" + i + " ready.");
+            new Downloader(i);
+            System.out.println("Downloader " + i + " ready.");
         }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (Thread t : Thread.getAllStackTraces().keySet()) {
+                if (t.getName().startsWith("Downloader")) {
+                    System.out.println(t + "Ending");
+                    t.interrupt();
+                }
+            }
+        }));
     }
 
     private static void print(String msg, Object... args) {
