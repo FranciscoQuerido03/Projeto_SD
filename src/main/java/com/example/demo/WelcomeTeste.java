0e3d0ba49ec80d97aa47e86b45b6ca4d8f144a95
Francisco Querido <148355011+FranciscoQuerido03@users.noreply.github.com>
2024-05-14 11:10:41 +0100

    Client identification Indexing Pages pointing to page

--- src/main/java/com/example/demo/WelcomeTeste.java
+++ src/main/java/com/example/demo/WelcomeTeste.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.forms.Project;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import sd_projeto.Client;
 import sd_projeto.Client_I;
 import sd_projeto.Message;
@@ -15,27 +16,40 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.UUID;
+import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class WelcomeTeste{
 
     public static Registry registry;
     public static Request Gateway;
-    public static Client c;
+    public static Map<String, Client> clientesAtivos = new HashMap<>(); // Mapa para armazenar os clientes ativos
+
 
     @GetMapping("/")
     public String welcome(Model model) {
         try {
             registry = LocateRegistry.getRegistry("localhost", 1098);
             Gateway = (Request) registry.lookup("request");
-            c = new Client();
+
+            //Cria um novo cliente e adiciona-o ao mapa de clientes ativos
+            Client c = new Client();
+            String clienteId = UUID.randomUUID().toString();
+            clientesAtivos.put(clienteId, c);
+
+            model.addAttribute("clienteId", clienteId);
+
+            System.out.println("Cliente criado com sucesso: " + clienteId);
+
             System.out.println("Inicializado com sucesso!!!");
         } catch (RemoteException | NotBoundException e) {
             System.out.println("Inicializado sem sucesso!!!");
@@ -46,7 +60,32 @@ public class WelcomeTeste{
 
     @GetMapping("/index")
     public String index(Model model) {
+        Query m = new Query();
+        model.addAttribute("query", m);
+//        Gateway.send_request_queue(c, conteudo);
         return "index";
+    }
+
+    @GetMapping("/indexing")
+    public String indexing(@ModelAttribute Query pesquisa, Model model) throws RemoteException {
+        try{
+            String clientId = pesquisa.getClientId();
+            Client c = clientesAtivos.get(clientId);
+
+            Message querry = new Message(pesquisa.getContent());
+
+            String content = "Indexação realizada com sucesso!";
+            model.addAttribute("content", content);
+
+            Gateway.send_request_queue(c, querry);
+
+            }catch (RemoteException e){
+                System.out.println("=======================================");
+                e.printStackTrace();
+            }
+
+
+        return "indexed";
     }
 
     @GetMapping("/search")
@@ -59,19 +98,66 @@ public class WelcomeTeste{
         return "search";
     }
 
-
     @GetMapping("/search_result")
     public String search_result(@ModelAttribute Query pesquisa, Model model) {
-
         try{
-            //Message querry = new Message(message.getContent());
-            ArrayList<URL_Content> content = Gateway.request10((Client_I) c, new Message(pesquisa.getContent()), 0);
+        String clientId = pesquisa.getClientId();
+        Client c = clientesAtivos.get(clientId);
+
+        Message querry = new Message(pesquisa.getContent());
+        ArrayList<URL_Content> content = Gateway.request10((Client_I) c, querry, 0);
+
+        if(content.get(0).title.equals("Falha Ocurrida")){
+            content = new ArrayList<>();
+//                content.get(0).title = "Universidade de Coimbra";
+//                content.get(0).url = "www.uc.pt";
+//                content.get(0).citacao = "Universidade de Coimbra";
+        }
+
+        model.addAttribute("content", content);
+
+
+        for(URL_Content u : content)
+            System.out.println(u.title + " " + u.url);
+
+        } catch (RemoteException e){
+            System.out.println("=======================================");
+            e.printStackTrace();
+        }
+
+
+        return "search_results";
+    }
+
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        return "info";
+    }
+
+    @GetMapping("/pages")
+    public String pages(Model model) {
+
+        Query m = new Query();
+        model.addAttribute("query", m);
+
+        return "pages";
+    }
+
+    @GetMapping("/pages_result")
+    public String pages_result(@ModelAttribute Query pesquisa, Model model) {
+        try{
+            String clientId = pesquisa.getClientId();
+            Client c = clientesAtivos.get(clientId);
+
+            Message querry = new Message(pesquisa.getContent());
+            ArrayList<URL_Content> content = Gateway.request10((Client_I) c, querry, 0);
 
             if(content.get(0).title.equals("Falha Ocurrida")){
                 content = new ArrayList<>();
-                //content.get(0).title = "Universidade de Coimbra";
-                //content.get(0).url = "www.uc.pt";
-                //content.get(0).citacao = "Universidade de Coimbra";
+//                content.get(0).title = "Universidade de Coimbra";
+//                content.get(0).url = "www.uc.pt";
+//                content.get(0).citacao = "Universidade de Coimbra";
             }
 
             model.addAttribute("content", content);
@@ -85,18 +171,8 @@ public class WelcomeTeste{
             e.printStackTrace();
         }
 
-        return "search_results";
-    }
 
-
-    @GetMapping("/info")
-    public String info(Model model) {
-        return "info";
-    }
-
-    @GetMapping("/pages")
-    public String pages(Model model) {
-        return "pages";
+        return "pages_results";
     }
 
     @GetMapping("/hackernews")
--- src/main/java/sd_projeto/Query.java
+++ src/main/java/sd_projeto/Query.java
@@ -2,16 +2,26 @@ package sd_projeto;
 
 public class Query {
     private String content;
+    private String clientId;
 
     public Query(){
         this.content = new String();
+        this.clientId = new String();
     }
 
     public String getContent() {
         return content;
     }
 
+    public String getClientId() {
+        return clientId;
+    }
+
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
     }
 }
